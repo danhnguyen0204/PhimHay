7b22f95af10bfd5e25a153f649d207d2c232baf1
unknown <nguyndanhthanh0204@gmail.com>
2025-03-30 12:00:09 +0700

    làm chức năng đăng ký

---
+++ js/signup.js
@@ -0,0 +1,5 @@
+function register(){
+    let username = document.getElementById('username').value.trim();
+    let password = document.getElementById('password').value.trim();
+    let fullname = document.getElementById('fullname').value.trim();
+}
